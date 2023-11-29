a53fe840ad42f0dd33f3f30e34f145b269dd7fab
stas224 <stas224stas@gmail.com>
2023-11-29 23:54:12 +0300

    fix display of output text

--- annotation_generation/hello_page/views.py
+++ annotation_generation/hello_page/views.py
@@ -8,8 +8,8 @@ def method(request):
     output_text = ''
     if request.method == 'POST':
         input_text = request.POST.get('input_text', '')
-        output_text = generate_response(input_text)
-
+        annotation, plan = generate_response(input_text)
+        output_text = 'Аннотация:\n{}\nПлан:\n{}'.format(annotation, plan)
     context = {'output_text': output_text}
 
     return render(request, 'index.html', context)
